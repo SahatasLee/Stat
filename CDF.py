0cd8f836ad5f3cbc30ef4127aa47c1182f9d66fa
SahatasLee <Sahataslee@outlook.co.th>
2021-02-22 12:30:13 +0700

    Vim exit nw

--- CDF.py
+++ CDF.py
@@ -17,7 +17,9 @@ glucose = y.to_list()
 bmi = z.to_list()
 
 data = np.array(bmi)
+data2 = np.array(glucose)
 data.sort()
+data2.sort()
 
 # https://www.youtube.com/watch?v=fQ0Iy0Sew_U
 
@@ -29,7 +31,20 @@ data.sort()
 # https://stackoverflow.com/questions/24788200/calculate-the-cumulative-distribution-function-cdf-in-python
 
 p = 1. * np.arange(len(data)) / (len(data) - 1)
-plt.plot(data, p)
+p2 = 1. * np.arange(len(data2)) / (len(data) - 1)
+
+fig, ax = plt.subplots(1, 2)
+fig.suptitle('Cumulative Probability Function')
+
+ax[0].plot(data, p)
+ax[0].set_title('BMI')
+ax[0].set_xlabel('BMI ((kg/height)^2)')
+ax[0].set_ylabel('Probability')
+
+ax[1].plot(data2, p2)
+ax[1].set_title('Glucose')
+ax[1].set_xlabel('Glucose (mmol/L)')
+ax[1].set_ylabel('Probability')
 
 
 plt.show()
--- PDF.py
+++ PDF.py
@@ -17,23 +17,39 @@ glucose = y.to_list()
 bmi = z.to_list()
 
 data = glucose
+data2 = bmi
 
 # calculate parameters
 sample_mean = stc.mean(data)
 sample_std = stc.stdev(data)
 print('Mean=%.3f, Standard Deviation=%.3f' % (sample_mean, sample_std))
 
+sample_mean2 = stc.mean(data2)
+sample_std2 = stc.stdev(data2)
+
 # define the distribution
 dist = norm(sample_mean, sample_std)
+dist2 = norm(sample_mean2, sample_std2)
 
 # sample probabilities for a range of outcomes
 values = [value for value in range(30, 200)]
 probabilities = [dist.pdf(value) for value in values]
 
-# plot the histogram and pdf
-plt.hist(data, bins=10, density=True)
-plt.plot(values, probabilities)
+values2 = [value2 for value2 in range(0, 70)]
+probabilities2 = [dist2.pdf(value2) for value2 in values2]
 
-plt.xlabel('Glucose (mmol/L)')
-plt.ylabel('Quality')
+fig, ax = plt.subplots(1, 2)
+fig.suptitle('Probability Density Function')
+
+# plot the histogram and pdf
+ax[0].hist(data, bins=10, density=True)
+ax[0].plot(values, probabilities)
+ax[0].set_xlabel('Glucose (mmol/L)')
+ax[0].set_ylabel('Probability')
+
+ax[1].hist(data2, bins=10, density=True)
+ax[1].plot(values2, probabilities2)
+ax[1].set_xlabel('BMI ((kg/height)^2)')
+ax[1].set_ylabel('Probability')
+
 plt.show()
